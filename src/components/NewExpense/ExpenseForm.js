6fd9dc03282e594c34047b39cb4c76bea6af473c
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2023-03-21 04:27:41 +0400

    Use multiple states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,11 +1,20 @@
 import './ExpenseForm.css';
+import { useState } from 'react';
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value)
+    setEnteredTitle(event.target.value);
   };
-  const amountChangeHandler = (event) => {};
-  const dateChangeHandler = (event) => {};
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
 
   return (
     <form>
