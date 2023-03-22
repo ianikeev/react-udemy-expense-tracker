64760f96df463b62465e3ebfb3c510a0be3f04d4
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2023-03-23 01:16:57 +0400

    Show expense chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,15 +2,20 @@ import './Chart.css';
 import ChartBar from './ChartBar';
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map(item => item.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
   return (
     <div className="chart">
-      {props.dataPoints.map((dataPoint) => (
-        <ChartBar
-          key={dataPoint.label}
-          label={dataPoint.label}
-          value={dataPoint.value}
-          maxValue={null}
-        />))}
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            label={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={totalMaximum}
+          />);
+      })}
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import ExpenseFilter from './ExpensesFilter';
 import { useState } from 'react';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [selectedYear, setSelectedYear] = useState('2020');
@@ -15,11 +16,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
+
   return (
     <li>
       <Card className="expenses">
         <ExpenseFilter selected={selectedYear}
                        onSelectExpenseYear={selectExpenseYearHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />;
       </Card>
     </li>
